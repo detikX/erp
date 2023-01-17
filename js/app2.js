a38c53a2132f6a406a95d0e7b1765a91fe2d2d05
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-01-17 10:07:50 +0700

    dededed

---
+++ js/app2.js
@@ -0,0 +1,24 @@
+$(".click").click(function(){
+    $(".hiden").show();
+    // $("#jalan").addClass('.lala')
+})
+
+var triangle = document.getElementById("triangle");
+var length = triangle.getTotalLength();
+
+triangle.style.strokeDasharray = length;
+
+// Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
+triangle.style.strokeDashoffset = length;
+
+// Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+window.addEventListener("scroll", myFunction);
+
+function myFunction() {
+var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+  var draw = length * scrollpercent;
+  
+  // Reverse the drawing (when scrolling upwards)
+  triangle.style.strokeDashoffset = length - draw;
+}
