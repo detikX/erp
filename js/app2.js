f42296b1a5799c54a41b543cedab0e3735ff6a72
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-01-19 05:15:04 +0700

    deded

--- js/app2.js
+++ js/app2.js
@@ -3,22 +3,22 @@ $(".click").click(function(){
     // $("#jalan").addClass('.lala')
 })
 
-var triangle = document.getElementById("triangle");
-var length = triangle.getTotalLength();
+// var triangle = document.getElementById("triangle");
+// var length = triangle.getTotalLength();
 
-triangle.style.strokeDasharray = length;
+// triangle.style.strokeDasharray = length;
 
-// Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
-triangle.style.strokeDashoffset = length;
+// // Hide the triangle by offsetting dash. Remove this line to show the triangle before scroll draw
+// triangle.style.strokeDashoffset = length;
 
-// Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
-window.addEventListener("scroll", myFunction);
+// // Find scroll percentage on scroll (using cross-browser properties), and offset dash same amount as percentage scrolled
+// window.addEventListener("scroll", myFunction);
 
-function myFunction() {
-var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+// function myFunction() {
+// var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
-  var draw = length * scrollpercent;
+//   var draw = length * scrollpercent;
   
-  // Reverse the drawing (when scrolling upwards)
-  triangle.style.strokeDashoffset = length - draw;
-}
+//   // Reverse the drawing (when scrolling upwards)
+//   triangle.style.strokeDashoffset = length - draw;
+// }
